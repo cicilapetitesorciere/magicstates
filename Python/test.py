426c1551ab83f7c88477331285f371fe614e0b47
Athena Caesura <mathmeetsmusic@gmail.com>
2024-01-05 11:20:34 -0500

    feat: improve error description

--- Python/test.py
+++ Python/test.py
@@ -5,7 +5,6 @@ mp.prec = 4096
 from onelevel15to1 import cost_of_one_level_15to1
 from twolevel15to1 import cost_of_two_level_15to1
 
-
-# When running this line, pphys=.001 and pphys=.0001 give the same result
+# both of these no longer match the paper
 cost_of_one_level_15to1(0.0001, 11, 5, 5)
 # cost_of_two_level_15to1(0.001, 13, 5, 5, 27, 13, 15, 4)
